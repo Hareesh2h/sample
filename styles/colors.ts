473cb54cc03beaadc9049edd6f3a9b6db33c39ac
Hareesh2h <63667815+Hareesh2h@users.noreply.github.com>
2025-05-13 15:59:18 +0530

    Add files via upload

---
+++ styles/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: '#405DE6',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#8E8E8E',
+  lightGray: '#DBDBDB',
+  red: '#ED4956',
+  transparent: 'transparent',
+  background: '#FAFAFA',
+};
